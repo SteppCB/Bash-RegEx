a7b93ba530fee0b9f744f8c0ebf3b8f78397e80e
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-09-27 15:10:05 -0400

    git commit -m "finished test filter"

--- 1-filter.sh
+++ 1-filter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 # Define the input file
 input_file="email_list.txt"
-
-# Use grep to filter and display email addresses ending with "@gmail.com" (you must use a regular expression for this)
+# Use grep to filter and display email addresses ending with "@gmail.com"
+grep -E "[a-zA-Z0-9._%+-]+@gmail\.com$" "$input_file"
 
